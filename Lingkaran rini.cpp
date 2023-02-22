4e0c3586ed5214cddccd213c31fd6c023915637f
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-02-22 09:02:31 +0700

    Add files via upload

---
+++ Lingkaran rini.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float luas, w, pi = 3.14; 
+	
+	cout<<"menghitung luas lingkaran\n\n"; 
+	cout<<"masukan jari jari : "; 
+	
+	cin>>w;
+	luas= pi*w*w; 
+	cout<<"luas lingkarannya adalah :"<<luas;
+	
+	return 0; 
+}
